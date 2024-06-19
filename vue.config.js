8a0de73dfa9f7b6dbec015225d56d5c2cb470432
ygorxds <ygordesa2010@hotmail.com>
2024-06-19 20:31:11 -0300

    debbug: debbug cors

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,16 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://34.203.248.169:5000',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 });
